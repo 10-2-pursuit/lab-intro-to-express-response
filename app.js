436235f0d5aa2e60c908fac85beef356b82f00c7
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-04 07:41:02 -0400

    Add '/magic8' route.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
 	res.send('Intro to Express Response Lab 🖖');
 });
 
+// Catchphrases Express
 // 1.
 app.get('/terminator', (req, res) => {
 	res.send(`I'll be back`);
@@ -63,6 +64,36 @@ app.get('/dorothy', (req, res) => {
 // 11.
 app.get('/terminator2', (req, res) => {
 	res.send(`Hasta la vista, baby`);
+});
+
+// Magic 8 Ball
+const magic8Responses = [
+	'It is certain',
+	'It is decidedly so',
+	'Without a doubt',
+	'Yes - Definitely',
+	'You may rely on it',
+	'As I see it, yes',
+	'Most likely',
+	'Outlook good',
+	'Yes, and signs point to yes',
+	'Reply hazy, try again',
+	'Ask again later',
+	'Better not tell you now',
+	'Cannot predict now',
+	'Concentrate and ask again',
+	"Don't count on it",
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful',
+];
+
+app.get('/magic8', (req, res) => {
+	const randomIndex = Math.trunc(Math.random() * magic8Responses.length);
+	const randomResponse = magic8Responses[randomIndex];
+
+	res.send(`<h1>${randomResponse}</h1>`);
 });
 
 // Listen

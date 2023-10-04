8b34212283e72ee41dc9dbfd640718d1877d8891
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-10-04 07:42:58 -0400

    Final commit.

--- app.js
+++ app.js
@@ -51,8 +51,33 @@ app.get("/Fox-Mulder", (req, res) => {
   res.send("The truth is out there");
 });
 
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
 app.get("/magic8", (req, res) => {
-  res.send(`<h1>${magic8Answer}</h1>`);
+  const randomIndex = Math.trunc(Math.random() * magic8Responses.length);
+  const randomResponse = magic8Responses[randomIndex];
+
+  res.send(`<h1>${randomResponse}</h1>`);
 });
 
 app.listen(PORT, () => {

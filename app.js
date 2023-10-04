441f70ec26ab702df4088c14ef95f2d2ea00b0bb
arim83 <jorammercado@pursuit.org>
2023-10-03 22:34:01 -0400

    magic8 done

--- app.js
+++ app.js
@@ -52,3 +52,30 @@ app.get('/fraiser', (request, response) => {
 app.listen(PORT, () => {
     console.log(`listening on prt ${PORT}`)
 })
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ]
+
+  app.get("/magic8", (request, response) => {
+    const randomIndex = Math.floor(Math.random()*magic8Responses.length)
+    response.send(`<h1>${magic8Responses[randomIndex]}</h1>`);
+});

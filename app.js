fb99d895d598f532bf9fba561cbe45feb1810cfd
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-04 01:55:12 -0400

    created route paths  with persons name and their catchphrase as their response

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello, Express!');
 }); 
 
+app.get('/terminator', (req, res) => { 
+    res.send("i'll be back");
+});
+
+app.get('/emiril', (req, res) => { 
+    res.send("iBam!");
+});
+
+app.get('/steve-mcgarrett', (req, res) => { 
+    res.send("Book 'em Danno!");
+});
+
 const port = process.env.PORT || 3000; 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);

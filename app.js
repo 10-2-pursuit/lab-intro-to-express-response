2cde0920d730899b8c134c72ec7a29889e6e205d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-04 07:08:28 -0400

    Add '/jj-evans' route.

--- app.js
+++ app.js
@@ -10,20 +10,29 @@ app.get('/', (req, res) => {
 	res.send('Intro to Express Response Lab 🖖');
 });
 
+// 1.
 app.get('/terminator', (req, res) => {
 	res.send(`I'll be back`);
 });
 
+// 2.
 app.get('/borg', (req, res) => {
 	res.send(`Resistance is futile`);
 });
 
+// 3.
 app.get('/bruce-banner', (req, res) => {
 	res.send(`Don't make me angry`);
 });
 
+// 4.
 app.get('/emeril', (req, res) => {
 	res.send(`Bam!`);
+});
+
+// 5.
+app.get('/jj-evans', (req, res) => {
+	res.send(`Dy-no-myte!`);
 });
 
 // Listen

7bb34f637ba16c7c4221e7d225fe5a7824666e87
Addis Jackson <addisjackson@pursuit.org>
2023-10-11 00:22:47 -0400

    write out individual paths

--- App.js
+++ App.js
@@ -68,6 +68,87 @@ app.get('/:name', (req, res) => {
 
 */
 
+app.get("/emeril" , (req, res) => {
+    res.send("Bam!")
+});
+app.get("/'Steve McGarrett'", (req, res) => {
+    res.send("Book 'em Danno!");
+})
+app.get("/Coach Taylor"), (req, res) => {
+    res.send("Clear eyes, full hearts, can't Lose");
+};
+app.get("/Homer Simpson", (req, res) => {
+    res.send("D'Oh");
+});  
+app.get("/Bruce Banner", (req, res) => {
+    res.send("Don't make me angry");
+});
+app.get("/JJ Evans", (req, res) => {
+    res.send("Dy-no-myte!");
+});
+app.get("/Batman", (req, res) => {
+    res.send("To the Batmobile!");
+});
+app.get("/Hannhausen Smith", (req, res) => {
+    res.send("I love it when a plan comes together");
+});
+app.get("/Fraiser", (req, res) => {
+    res.send("I'm listening");
+});
+app.get("/Regis", (req, res) => {
+    res.send("Is that your final answer?");
+});
+app.get("/Borg", (req, res) => {
+    res.send("Resistance is futile");
+});
+app.get("/Fox Mulder", (req, res) => {
+    res.send("The truth is out there");
+});
+app.get("/Harry Callahan", (req, res) => {
+    res.send("Go ahead, make my day");
+});
+app.get("/Travis Bickle", (req, res) => {
+    res.send("You talkin' to me?");
+});
+app.get("/Tony Montana", (req, res) => {
+    res.send("Say hello to my little friend");
+});
+app.get("/Zeus", (req, res) => {
+    res.send("Release the Kraken");
+});
+app.get("/James Bond", (req, res) => {
+    res.send("the name is Bond, James Bond");
+})
+app.get("Dorothy" , (req, res) => {
+    res.send("Toto, I've got a feeling we're not in Kansas anymore");
+});
+app.get("/Rod Tidwell", (req, res) => {
+    res.send("Show me the money!");
+});
+app.get("/Frankenstein" , (req, res) => {
+    res.send("It's alive! It's alive");
+});
+app.get("/Jim Lovell" , (req, res) => {
+    res.send("Houston, we have a problem");
+});
+app.get("/Rocky" , (req, res) => {
+    res.send("Yo, Adrian"); 
+});
+app.get("/Gollum" , (req, res) => {
+    res.send("My precious");
+});
+app.get("/Jack Dawson" , (req, res) => {
+    res.send("I'm king of the world!");
+});
+app.get("/Terminator" , (req, res) => {
+    res.send("Hasta la vista, baby");
+})
+
+
+/*app.get('terminator', (req, res) => {
+  res.send('I\ll be back')
+  res.send('Hasta la vista, baby')
+})*/
 
 
 const magic8Responses = [

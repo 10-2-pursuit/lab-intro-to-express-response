aef465f126a6dc891637c42b29e6b47beaad509b
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-21 09:36:25 -0400

    Add a bunch of other routes.

--- app.js
+++ app.js
@@ -11,8 +11,44 @@ app.get("/tim-gunn", (req, res) => {
   res.send("Make it work");
 });
 
-app.get("/terminator", (req, res) => {
-  res.send("I'll be back.");
+app.get("/emeril", (req, res) => {
+  res.send("Bam!");
+});
+
+app.get("/coach-taylor", (req, res) => {
+  res.send("Clear eyes, full hearts, can't Lose");
+});
+
+app.get("/homer-simpson", (req, res) => {
+  res.send("D'oh!");
+});
+
+app.get("/bruce-banner", (req, res) => {
+  res.send("Don't make me angry.");
+});
+
+app.get("/jj-evans", (req, res) => {
+  res.send("Dy-no-myte!");
+});
+
+app.get("/batman", (req, res) => {
+  res.send("To the Batmobile!");
+});
+
+app.get("/hannibal-smith", (req, res) => {
+  res.send("I love it when a plan comes together.");
+});
+
+app.get("/frasier", (req, res) => {
+  res.send("I'm listening.");
+});
+
+app.get("/regis", (req, res) => {
+  res.send("Is that your final answer?");
+});
+
+app.get("/borg", (req, res) => {
+  res.send("Resistance is futile.");
 });
 
 app.listen(PORT, () => {

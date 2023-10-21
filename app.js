9b593e11fa70eb67fd4651934e2c9e6df015f18a
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-21 09:40:49 -0400

    Add 8ball route.

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const express = require("express");
 const app = express();
 const PORT = 3003;
 
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -49,6 +71,10 @@ app.get("/regis", (req, res) => {
 
 app.get("/borg", (req, res) => {
   res.send("Resistance is futile.");
+});
+
+app.get("/magic8", (req, res) => {
+  res.send(magic8Responses[Math.floor(Math.random() * magic8Responses.length)]);
 });
 
 app.listen(PORT, () => {

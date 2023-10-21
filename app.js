e09119e945d27f994e5247adc226ffab37970d5d
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-21 09:29:55 -0400

    Add first quote routes.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/tim-gunn", (req, res) => {
+  res.send("Make it work");
+});
+
+app.get("/terminator", (req, res) => {
+  res.send("I'll be back.");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
